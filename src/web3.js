41202ce5314eb9d0ff84c5c15afdfc7f74af9397
Krzysztof Kaczor <krzkaczor@gmail.com>
2019-01-11 12:01:04 +0100

    some comments

--- src/web3.js
+++ src/web3.js
@@ -3,6 +3,7 @@ import { chain } from '../config/env';
 const Web3 = require('web3');
 
 function getProvider() {
+  // ganache works a little bit differently then other nodes so we provider different provider for websockets
   if (chain.id === "local") {
     return new Web3.providers.WebsocketProvider(chain.provider)
   } else {
